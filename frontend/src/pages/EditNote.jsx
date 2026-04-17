3916d6848d9e97cf81acacb85383893f4ddf9d37
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-04-17 17:15:13 +0530

    edit note created

---
+++ frontend/src/pages/EditNote.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import { useParams, useNavigate } from "react-router-dom";
+
+function EditNote() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        loadNote();
+    }, []);
+
+    const loadNote = async () => {
+        const res = await API.get(`/${id}`);
+        setTitle(res.data.title);
+        setContent(res.data.content);
+    };
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+
+        await API.put(`/${id}`, { title, content });
+        navigate("/");
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-lg">
+
+                <h2 className="text-2xl font-bold mb-6">Edit Note</h2>
+
+                <form onSubmit={handleUpdate} className="space-y-4">
+
+                    <input
+                        className="w-full p-3 border rounded-xl"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+
+                    <textarea
+                        className="w-full p-3 border rounded-xl h-32"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+
+                    <button className="w-full bg-green-600 text-white py-3 rounded-xl hover:bg-green-700">
+                        Update Note
+                    </button>
+
+                </form>
+
+            </div>
+        </div>
+    );
+}
+
+export default EditNote;
